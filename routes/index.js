7e9e50c24c284b17edab2e760597502f7bb69830
mdakbarhossain1 <akbarshovo18@gmail.com>
2024-03-01 10:22:37 +0600

    profile uploead and conditional rendering on nav bar

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,38 @@ var router = express.Router();
 const userModel = require("./users");
 const passport = require('passport');
 const localStrategy = require("passport-local");
+const upload = require("./multer");
 
 passport.use(new localStrategy(userModel.authenticate()));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index',{nav: false});
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('register');
+  res.render('register',{nav:false});
 });
 
-router.get('/profile', isLoggedIn, function(req, res, next) {
-  res.render('profile');
+router.get('/profile', isLoggedIn,async function(req, res, next) {
+  const user = await userModel.findOne({username: req.session.passport.user});
+
+  res.render('profile', {user, nav:true});
+
 });
+
+// File Upload Route 
+router.post('/fileupload', isLoggedIn, upload.single("image"),async function(req, res, next){
+  
+ const user = await userModel.findOne({username: req.session.passport.user})
+ user.profileImage = req.file.filename;
+
+ await user.save();
+ res.redirect("/profile");
+ 
+ // res.send(user)
+});
+
 
 //Register Route
 router.post('/register', function(req, res){
